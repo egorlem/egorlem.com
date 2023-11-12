30167173e583935e2ead79d2f1897d4ca6fecf36
guesswhozzz <torridtime@gmail.com>
2023-11-12 13:05:34 +0300

    Add #12 React deps

---
+++ src/app/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const App = () => {
+    return null
+    return (
+        <div>
+            <h1>hot module</h1>
+        </div>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 'use strict';
 import './styles/index.scss';
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./app";
+
+ReactDOM.render(<App />, document.getElementById("root"));
