cb2943e37101b99534da6af90e97c598347979bd
guesswhozzz <torridtime@gmail.com>
2023-12-04 15:37:56 +0300

    No fate

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,77 @@
-import React from "react";
+import React, { useEffect, useReducer } from 'react';
+import './style.scss';
+
+// l command 91 r command 93
+// option 18
+// i 73
+
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'incremented_age': {
+            return {
+                name: state.name,
+                age: state.age + 1
+            };
+        }
+        case 'changed_name': {
+            return {
+                name: action.nextName,
+                age: state.age
+            };
+        }
+    }
+    throw Error('Unknown action: ' + action.type);
+}
+
+const initialState = {
+    91: ''
+}
 
 export const App = () => {
     console.log('with react')
-    return null
+    // const [state, dispatch] = useReducer(reducer, initialState);
+    // return null
+
+    const handleKeyDown = (event) => {
+        const { keyCode } = event
+        console.log(document.activeElement)
+        console.log(typeof keyCode)
+    }
+
+    const nadleKeyUp = (event) => {
+        console.log(event)
+    }
+
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         console.log(document.activeElement);
+    //     }, 500)
+    // }, []);
+
     return (
         <div>
-            <h1>hot module</h1>
+            <button
+                onKeyDown={handleKeyDown}
+                onKeyUp={nadleKeyUp}
+                className='buttonKey'
+            >
+                option
+            </button>
+            <button
+                onKeyDown={handleKeyDown}
+                onKeyUp={nadleKeyUp}
+                className='buttonKey'
+            >
+                command
+            </button>
+            <button
+                onKeyDown={handleKeyDown}
+                onKeyUp={nadleKeyUp}
+                className='buttonKey'
+            >
+                i
+            </button>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
 'use strict';
+/* LIBS */
+import React from "react";
+import ReactDOM from "react-dom/client";
+/* COMPOENTS */
+import { App } from './app';
+/* STYLES */
 import './styles/index.scss';
 
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./app";
+// if( window.matchMedia('(prefers-color-scheme: dark)').matches )
+// {
+//     scheme.innerHTML = 'В ОС установлена тёмная тема';
+// }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// if( window.matchMedia('(prefers-color-scheme: light)').matches )
+// {
+//     scheme.innerHTML = 'В ОС установлена светлая тема';
+// }
+
+// console.log('init');
+
+// const rootReactNode = document.getElementById('root');
+
+// function initUserData() {
+//   console.log('data seted');
+// }
+
+// document.addEventListener('keyup', (event) => {
+//   const { code } = event;
+//   rootReactNode.innerText = keyCode + 'up'
+//   console.log(which);
+
+// })
+// document.addEventListener('keydown', (event) => {
+//   const { code } = event;
+//   rootReactNode.innerText = keyCode + 'down'
+// })
+
+// initUserData();
+
+// /* REACT */
+// const rootReactNode = document.getElementById('root');
+// const root = ReactDOM.createRoot(rootReactNode);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+
